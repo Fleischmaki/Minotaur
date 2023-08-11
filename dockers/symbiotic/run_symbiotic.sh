5eec98f18456e6277cb22f6580e078a5b7c48ec9
Fleischmaki <markus@losfleischmann.com>
2023-08-11 18:39:21 +0200

    Added Symbiotic

---
+++ dockers/symbiotic/run_symbiotic.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Arg1: Target Source code
+# Arg2: Timeout (in minutes)
+# Arg3: Variant
+
+
+WORKDIR=/home/maze/workspace
+
+INDIR=$WORKDIR/inputs
+OUTDIR=$WORKDIR/outputs
+OUTFILE=$OUTDIR/res
+
+mkdir -p $OUTDIR
+
+# Create dummy file to indicate running start
+touch $WORKDIR/.start
+let TIMEOUT=60*$2 
+#timeout $2m /home/maze/tools/symbiotic/scripts/symbiotic --exit-on-error $1 &> $OUTDIR/res 
+timeout $2m /opt/symbiotic/install/bin/symbiotic --timeout=$2 --no-integrity-check --explicit-symbolic --undefined-are-pure $1 &> $OUTDIR/res 
+touch $WORKDIR/.end
+
+mv output outputs/output
