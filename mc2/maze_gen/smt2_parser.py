b5e62536722bc117557baedebbe80490ef17808a
Markus <markus@losfleischmann.com>
2023-09-01 14:02:32 +0200

    Fixed smtparser for real

--- mc2/maze_gen/smt2_parser.py
+++ mc2/maze_gen/smt2_parser.py
@@ -40,44 +40,44 @@ def bits_to_type(n):
 def bits_to_utype(n):
     return "unsigned " + bits_to_type(n)
 
-#def cast_to_signed(l, r):
-#    cast = ""
-#    extend_step = 0
-#    if l.is_bv_constant():
-#        cast = "(" + bits_to_type(l.bv_width()) + ") "
-#    elif r.is_bv_constant():
-#        cast = "(" + bits_to_type(r.bv_width()) + ") "
-#    elif l.is_bv_sext() or l.is_bv_zext():
-#        extend_step = l.bv_extend_step()
-#    elif r.is_bv_sext() or r.is_bv_zext():
-##        extend_step = r.bv_extend_step()
-#    else:
-#        error(1,l,r)
-#        
-#    if extend_step in (8,16,24,32,48,56):
-#        cast = '(' + bits_to_type(extend_step+1) + ')'
-#    return cast
+def cast_to_signed(l, r):
+   cast = ""
+   extend_step = 0
+   if l.is_bv_constant() or l.is_symbol or l.is_function_application() or l.is_ite() or l.is_select():
+       cast = "(" + bits_to_type(l.bv_width()) + ") "
+   elif r.is_bv_constant() or r.is_symbol or r.is_function_application() or r.is_ite() or r.is_select():
+       cast = "(" + bits_to_type(r.bv_width()) + ") "
+   elif l.is_bv_sext() or l.is_bv_zext():
+       extend_step = l.bv_extend_step()
+   elif r.is_bv_sext() or r.is_bv_zext():
+        extend_step = r.bv_extend_step()
+   else:
+       error(1,l,r)
+       
+   if extend_step in (8,16,24,32,48,56):
+       cast = '(' + bits_to_type(extend_step+1) + ')'
+   return cast
 
-def get_cast(node,sign):
-    return '(' + ('unsigned ' if sign == 'u' else '') + str(bits_to_type(node.bv_width())) +')'
+#def get_cast(node,sign):
+#    return '(' + ('unsigned ' if sign == 'u' else '') + str(bits_to_type(node.bv_width())) +')'
     
-#    _to_unsigned(l, r):
-#    cast = ""
-#    extend_step = 0
-#    if l.is_bv_constant():
-#        cast = "(" + bits_to_utype(l.bv_width()) + ") "
-#    elif r.is_bv_constant():
-#        cast = "(" + bits_to_utype(r.bv_width()) + ") "
-#    elif l.is_bv_sext() or l.is_bv_zext():
-#        extend_step = l.bv_extend_step()
-#    elif r.is_bv_sext() or r.is_bv_zext():
-#        extend_step = r.bv_extend_step()
-#    else:
-#        error(1,l)
-#        
-#    if extend_step in (8,16,24,32,48,56):
-#        cast = '(' + bits_to_utype(extend_step+1) + ')'
-#    return cast
+def cast_to_unsigned(l, r):
+   cast = ""
+   extend_step = 0
+   if l.is_bv_constant():
+       cast = "(" + bits_to_utype(l.bv_width()) + ") "
+   elif r.is_bv_constant():
+       cast = "(" + bits_to_utype(r.bv_width()) + ") "
+   elif l.is_bv_sext() or l.is_bv_zext():
+       extend_step = l.bv_extend_step()
+   elif r.is_bv_sext() or r.is_bv_zext():
+       extend_step = r.bv_extend_step()
+   else:
+       error(1,l)
+       
+   if extend_step in (8,16,24,32,48,56):
+       cast = '(' + bits_to_utype(extend_step+1) + ')'
+   return cast
 
 def type_to_c(type):
     if type.is_bool_type():
@@ -96,7 +96,7 @@ def type_to_c(type):
 def convert_helper(symbs,node, cons, op, cast = '', cut_overflow = False):
     (l, r) = node.args()
     if cast != '':
-        cast = get_cast(node,cast)
+        cast = cast_to_unsigned if cast == 'u' else cast_to_unsigned
     if cut_overflow:
         cons.write('(' + bits_to_utype(node.bv_width()) + ')(')
     cons.write(cast)
