7241e36c717be645cd171bbfccdb0e8fb590137e
Markus <markus@losfleischmann.com>
2023-09-04 21:21:01 +0200

    Kill and remove gen-gen docker after use

--- mc2/tools/generate.py
+++ mc2/tools/generate.py
@@ -4,4 +4,5 @@ def load(argv):
     out_dir = argv[0]
     params = argv[1:]
     mg.generate_maze_in_docker(mg.get_params_from_string(" ".join(params)), 'gen').wait()
-    return docker.copy_docker_results('gen','gen',out_dir)
+    docker.copy_docker_results('gen','gen',out_dir).wait()
+    return docker.kill_docker('gen','gen')
