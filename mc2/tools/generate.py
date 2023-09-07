732117c2276aa02e02066c16d66316bb63a97f2f
Fleischmaki <markus@losfleischmann.com>
2023-09-07 21:40:01 +0200

    Fixed --g

--- mc2/tools/generate.py
+++ mc2/tools/generate.py
@@ -4,5 +4,5 @@ def load(argv):
     out_dir = argv[0]
     params = argv[1:]
     mg.generate_maze_in_docker(mg.get_params_from_string(" ".join(params)), 'gen').wait()
-    docker.copy_docker_results('gen','gen',out_dir).wait()
-    return docker.kill_docker('gen','gen')
+    docker.copy_docker_results('gen','gen',out_dir)
+    return docker.kill_docker('gen','gen').wait()
