f60573e125475c4a90d7c5eeb32f12e89aacc4cd
Fleischmaki <markus@losfleischmann.com>
2023-09-10 17:02:58 +0200

    Add mopsa

---
+++ dockers/mopsa/run_mopsa.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Arg1: Target Source code
+# Arg2: Timeout (in minutes)
+# Arg3: Variant
+
+sudo chown -R maze:maze $1
+
+WORKDIR=/home/maze/workspace
+
+INDIR=$WORKDIR/inputs
+OUTDIR=$WORKDIR/outputs
+OUTFILE=$OUTDIR/res
+
+mkdir -p $OUTDIR
+
+# Create dummy file to indicate running start
+touch $WORKDIR/.start
+timeout --foreground $2m /home/maze/tools/mopsa-analyzer/bin/mopsa -c-check-unsigned-implicit-cast-overflow false -no-color -config c/$3.json -additional-stubs c/mopsa/svcomp.c $1 >  $OUTDIR/res 
+touch $WORKDIR/.end
