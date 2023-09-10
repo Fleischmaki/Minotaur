d6accc776e4ab80c7d55e0cf15dac7e09a6ba00c
Fleischmaki <markus@losfleischmann.com>
2023-09-10 17:02:45 +0200

    Changed array type to signed long

--- mc2/maze_gen/smt2_parser.py
+++ mc2/maze_gen/smt2_parser.py
@@ -85,7 +85,7 @@ def type_to_c(type):
     elif type.is_function_type():
         return type_to_c(type.return_type)
     elif type.is_array_type():
-        return 'unsigned long' # otherwise store might be unsound, we can always cast afterwards
+        return 'long' # otherwise store might be unsound, we can always cast afterwards
     elif type.is_string_type():
         return 'string'
     else:
