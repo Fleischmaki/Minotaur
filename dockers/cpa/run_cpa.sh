1dd2ac57d534cb46cdbd56a0398b51e6dce0d882
Fleischmaki <markus@losfleischmann.com>
2023-09-16 17:48:34 +0200

    Added Mopsa to scripts, config

--- dockers/cpa/run_cpa.sh
+++ dockers/cpa/run_cpa.sh
@@ -20,5 +20,3 @@ let TIMEOUT=60*$2
 # Tell CPA how long it has (to select heuristics, etc.), but use timeout to ensure termination
 timeout $2m /home/maze/tools/cpa/scripts/cpa.sh -spec sv-comp-reachability -preprocess -$3 -timelimit $TIMEOUT $1 &> $OUTDIR/res 
 touch $WORKDIR/.end
-
-mv output outputs/output
