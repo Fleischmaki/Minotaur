a2dacdbb443eb105118fc72d81427b40ababd2b3
Fleischmaki <markus@losfleischmann.com>
2023-10-03 18:44:28 +0200

    Add optional local gen to --g

--- src/tools/generate.py
+++ src/tools/generate.py
@@ -1,8 +1,15 @@
 from ..runner import docker, maze_gen as mg
 
 def load(argv):
-    out_dir = argv[0]
-    params = argv[1:]
-    mg.generate_maze_in_docker(mg.get_params_from_string(" ".join(params)), 'gen').wait()
-    docker.copy_docker_results('gen','gen',out_dir)
-    return docker.kill_docker('gen','gen').wait()
+    gen = argv[0]
+    out_dir = argv[1]
+    params = argv[2:]
+    param_string = mg.get_params_from_string(" ".join(params))
+    if gen == 'container':
+        mg.generate_maze_in_docker(param_string, 'gen').wait()
+        docker.copy_docker_results('gen','gen',out_dir)
+        return docker.kill_docker('gen','gen').wait()
+    elif gen == 'local': 
+        mg.generate_maze(param_string, out_dir)
+    else:
+        print("Invalid generation option, please use one of [container,local]")
