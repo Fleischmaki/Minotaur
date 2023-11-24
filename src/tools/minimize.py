228a065d11110e287541cdf1ac2ac7a28d8d2cfd
Fleischmaki <markus@losfleischmann.com>
2023-11-24 10:37:05 +0100

    Add option to read params from summary.csv

--- src/tools/minimize.py
+++ src/tools/minimize.py
@@ -1,21 +1,23 @@
 import os
 from ..runner import *
 from ..maze_gen import smt2_parser as sp
+from math import ceil
 
-def main(maze_path,seeddir, outdir,timeout,gen,err,tool,variant,flags):
-    params = get_params(maze_path,seeddir)
+def main(params, outdir,timeout,gen,err,tool,variant,flags):
     commands.run_cmd('mkdir -p %s' % outdir)
     commands.run_cmd("mkdir -p %s" % os.path.join(outdir,'seeds'))
     commands.run_cmd("mkdir -p %s" % os.path.join(outdir,'runs'))
 
-    if not not_err(err,tool, variant, flags, params, outdir, timeout,gen):
+    if not result_is_err(err,tool, variant, flags, params, outdir, timeout,gen):
         print('ERROR: Original not maze not a %s' % err)
         return
 
     if not 'u' in params.keys():
         params.update({'u':''})
-        if not not_err(err,tool, variant, flags, params, outdir, timeout, gen):
+        if not result_is_err(err,tool, variant, flags, params, outdir, timeout, gen):
             params.pop('u', None) 
+        else:
+            params.update({'w':1,'h':1})
 
     mutant = os.path.join(outdir,'smt', 'mutant_%d.smt2' % (params['m'] - 1))
     clauses = get_clauses(mutant)
@@ -30,7 +32,7 @@ def main(maze_path,seeddir, outdir,timeout,gen,err,tool,variant,flags):
 
         seed = os.path.join(outdir, 'seeds', str(half) + ('-first' if keep_first_half else '-second'))
         set_seed(params,seed,new_clauses)
-        misses_bug = not_err(err,tool, variant, flags, params, outdir,timeout,gen)
+        misses_bug = result_is_err(err,tool, variant, flags, params, outdir,timeout,gen)
 
         commands.run_cmd('rm -r %s' % os.path.join(outdir, 'src'))
         if misses_bug:
@@ -49,7 +51,7 @@ def main(maze_path,seeddir, outdir,timeout,gen,err,tool,variant,flags):
         clause = clauses.pop(pos)
     
         set_seed(params,seed,clauses)
-        misses_bug = not_err(err,tool,variant,flags,params, outdir,timeout,gen)
+        misses_bug = result_is_err(err,tool,variant,flags,params, outdir,timeout,gen)
         commands.run_cmd('rm -r %s' % os.path.join(outdir, 'src'))
         
         if misses_bug:
@@ -64,9 +66,9 @@ def main(maze_path,seeddir, outdir,timeout,gen,err,tool,variant,flags):
     else:
         maze_gen.generate_maze(params, outdir)    
 
-def not_err(err,tool,variant,flags,params, outdir, timeout,gen):
+def result_is_err(err,tool,variant,flags,params, outdir, timeout,gen):
     docker.run_mc(tool,variant,flags, 'min', params, outdir,timeout=timeout, gen=gen)
-    sat = is_fn(outdir,err)
+    sat = is_err(outdir,err)
     return sat
 
 def set_seed(params, seed, clauses):
@@ -77,10 +79,10 @@ def set_seed(params, seed, clauses):
 
 
 def get_clauses(mutant):
-    formula = sp.read_file(mutant)[3]
+    formula = sp.read_file(mutant)[2]
     return list(sp.conjunction_to_clauses(formula))
 
-def is_fn(outdir,err):
+def is_err(outdir,err):
     resdir = os.path.join(outdir,'res')
     for file in os.listdir(resdir):
             if '_' in file: # Still false negative
@@ -106,15 +108,33 @@ def set_fake_params(params):
 
 
 def load(argv):
-    maze = argv[0]
-    seeddir = argv[1]
-    outdir = argv[2]
-    timeout = argv[3]
-    gen = argv[4]
-    err = argv[5]
-    tool = argv[6]
-    variant = flags = ''
-    if len(argv) >= 8:
-        variant = argv[7]
-        flags = ' '.join(argv[8:])
-    main(maze,seeddir,outdir,timeout,gen, err, tool, variant, flags)
+    if ',' in argv[0]:
+        run = argv[0][:-1] if argv[0].endswith(',') else argv[0]
+        if len(run.split(',')) == 15:
+            tool,variant,flags,id,u,a,w,h,c,t,g,_,r,timeout,err = run.split(',')
+        else:
+            tool,variant,flags,id,a,w,h,c,t,g,_,r,timeout,err = run.split(',')
+            u = 0
+
+        timeout = ceil(float(timeout)/60) + 1
+        if u == '1':
+            params['u'] = ''
+        seeddir = argv[1]
+        outdir = argv[2]
+        gen = argv[3]
+        params = {'m':int(id),'a':a,'w':int(w),'h':int(h),'c':int(c),'t':t,'g':'CVE_gen','s':os.path.join(seeddir,g+'.smt2'),'r':int(r)}
+    else:
+        maze = argv[0]
+        seeddir = argv[1]
+        outdir = argv[2]
+        timeout = argv[3]
+        gen = argv[4]
+        err = argv[5]
+        tool = argv[6]
+        variant = flags = ''
+        if len(argv) >= 8:
+            variant = argv[7]
+            flags = ' '.join(argv[8:])
+        params = get_params(maze,seeddir)
+
+    main(params,outdir,timeout,gen, err, tool, variant, flags)
