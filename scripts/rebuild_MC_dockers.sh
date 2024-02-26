a7105d014ddabe966fc900ef64221d4395516f83
Fleischmaki <markus@losfleischmann.com>
2024-02-26 18:20:59 +0100

    Cleanup

--- scripts/rebuild_MC_dockers.sh
+++ scripts/rebuild_MC_dockers.sh
@@ -5,5 +5,5 @@ set -e
 DOCKERDIR=$(readlink -f $(dirname "$0")/..)/dockers
 
 for tool in base  cbmc cpa  seahorn gen ultimate-release cpa-release mopsa ultimate 2ls symbiotic esbmc;
-    do echo "[*] Build minotaur-$tool Docker image..."; cd "$DOCKERDIR/$tool"; docker build --no-cache --rm -t minotaur$tool .; echo "[*] Done!";
+    do echo "[*] Build minotaur-$tool Docker image..."; cd "$DOCKERDIR/$tool"; docker build --no-cache --rm -t minotaur-$tool .; echo "[*] Done!";
 done; 
