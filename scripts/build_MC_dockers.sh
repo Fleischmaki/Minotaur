36af866dea43627503c8614294798dd21ebac517
Fleischmaki <markus@losfleischmann.com>
2024-03-19 17:40:42 +0100

    Add esbmc base to docker script

--- scripts/build_MC_dockers.sh
+++ scripts/build_MC_dockers.sh
@@ -4,6 +4,6 @@ set -e
 
 DOCKERDIR=$(readlink -f $(dirname "$0")/..)/dockers
 
-for tool in base cbmc cpa  seahorn gen ultimate-release cpa-release mopsa ultimate 2ls symbiotic esbmc;
+for tool in base esbmc-base cbmc cpa  seahorn gen ultimate-release cpa-release mopsa ultimate 2ls symbiotic esbmc;
     do echo "[*] Build minotaur-$tool Docker image..."; cd "$DOCKERDIR/$tool"; docker build --rm -t minotaur-$tool .; echo "[*] Done!";
 done; 
