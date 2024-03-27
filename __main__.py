ac1285481d32f84bfbcc537547c04715343b0200
Fleischmaki <markus@losfleischmann.com>
2024-03-27 16:11:49 +0100

    Set log level in main

--- __main__.py
+++ __main__.py
@@ -3,7 +3,6 @@ import sys, logging
 LOGGER = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO)
     mode = sys.argv[1]
     if not mode.startswith("--"):
         mode = input("Run tests [t], experiment [e], generate maze [g] or minimize [m]")
@@ -11,6 +10,21 @@ if __name__ == '__main__':
     else:
         mode = mode[2:]
         argv = sys.argv[2:]
+    log = argv[0]
+    if not log.startswith("--"):
+        log = logging.INFO
+    else:
+        log = log[2:]
+        if log == 'E':
+            log = logging.ERROR
+        elif log == 'W':
+            log = logging.WARNING
+        elif log == 'I':
+            log = logging.INFO
+        elif log == 'D':
+            log = logging.DEBUG
+        argv = argv[1:]
+    logging.basicConfig(level=log, format='%(levelname)s: %(message)s', style='%')
 
     if mode == "t":
         test.load(argv)
