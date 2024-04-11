efe5e06309074f32e71b85c00aea39f4ee4b3172
Markus Fleischmann <markus@losfleischmann.com>
2024-04-11 09:08:55 +0200

    Update coverage commands

--- dockers/esbmc/get_coverage.sh
+++ dockers/esbmc/get_coverage.sh
@@ -3,4 +3,4 @@
 # ARG 2: Outfile
 ESBMC_DIR="/home/tools/esbmc/src"
 mkdir -p "$1"
-python3 -m gcovr -v -r $ESBMC_DIR/src/ --filter=$ESBMC_DIR/src/ --filter=$ESBMC_DIR/build/src --json $ESBMC_DIR/build/src/esbmc
+python3 -m gcovr -v -r $ESBMC_DIR/src/ --filter=$ESBMC_DIR/src/ --filter=$ESBMC_DIR/build/src --json "$1/$2".json $ESBMC_DIR/build/src/esbmc
