18aa7a85590a1269b0b3f8b3167673cdff6c1158
Markus Fleischmann <markus@losfleischmann.com>
2024-04-23 14:40:00 +0200

    Add gen to experiment dockers

--- scripts/build_experiment_dockers.sh
+++ scripts/build_experiment_dockers.sh
@@ -4,6 +4,6 @@ set -e
 
 DOCKERDIR=$(readlink -f $(dirname "$0")/..)/dockers
 
-for tool in base esbmc-base ultimate-release cpa-release ultimate-bug2  esbmc-bug1 esbmc-bug2 esbmc-bug3;
+for tool in base gen ultimate-release cpa-release ultimate-bug2 esbmc-base esbmc-bug1 esbmc-bug2 esbmc-bug3;
     do echo "[*] Build minotaur-$tool Docker image..."; cd "$DOCKERDIR/$tool"; docker build --rm -t minotaur-$tool .; echo "[*] Done!";
 done; 
