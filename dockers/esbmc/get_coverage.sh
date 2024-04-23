d1664dd469edb7e13c344962d100a4dfae0b4184
Markus Fleischmann <markus@losfleischmann.com>
2024-04-23 10:29:24 +0200

    Fux esbmc coverage

--- dockers/esbmc/get_coverage.sh
+++ dockers/esbmc/get_coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # ARG 1: Outdir
 # ARG 2: Outfile
-ESBMC_DIR="/home/tools/esbmc/src"
+ESBMC_DIR="/home/maze/tools/esbmc"
 mkdir -p "$1"
-python3 -m gcovr -v -r $ESBMC_DIR/src/ --filter=$ESBMC_DIR/src/ --filter=$ESBMC_DIR/build/src --json "$1/$2".json $ESBMC_DIR/build/src/esbmc
+python3 -m gcovr -r "$ESBMC_DIR/src" --filter="$ESBMC_DIR/src" --filter="$ESBMC_DIR/build/src" --json "$1/$2".json "$ESBMC_DIR/build/src/esbmc"
