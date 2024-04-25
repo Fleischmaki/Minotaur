e95645be2440869344483b178b3ad9430987e150
Fleischmaki <markus@losfleischmann.com>
2024-04-25 15:52:08 +0200

    get_average_times should only include times where seed is found

--- scripts/get_average_times.py
+++ scripts/get_average_times.py
@@ -1,4 +1,4 @@
-from numpy import average
+from numpy import NaN, average
 import pandas
 import sys
 import os
@@ -6,17 +6,17 @@ run_directory = sys.argv[1]
 df = pandas.read_csv(os.path.join(run_directory,'times'), header='infer')
 num_tests = int(sys.argv[2])
 total_runs = len(df.iloc[:,1])
-runs_per_test = num_tests/total_runs
 
-for test in range(num_tests):
-    avg = (average(df.iloc[test::num_tests,1]))
+for test_id in range(num_tests):
     tool_times = []
-    found = 0
-    for run in range(test,total_runs,num_tests):
+    found = [False] * total_runs
+    for run in range(test_id, total_runs, num_tests):
         summary = pandas.read_csv(os.path.join(run_directory, f'run{run}_{0}','summary.csv'), header='infer')
-        tool_times.append(sum(summary['runtime'][::]))
         if any(map(lambda res: res in ('fn', 'fp'), summary['status'][::])):
-            found += 1
+            found[run] = True
+            tool_times.append(sum(summary['runtime'][::]))
+    avg = average(df.iloc[found,1])
+    
+    avg_tool_time = NaN if len(tool_times) == 0 else average(tool_times)
 
-    avg_tool_time = average(tool_times)
-    print(f"Test {test}:\tTotal time {avg:.2f},\t Tool time {avg_tool_time:.2f},\tPA% = {100*avg_tool_time / avg:.3f}\t found {found}/5")
+    print(f"Test {test_id}:\tTotal time {avg:.2f},\t Tool time {avg_tool_time:.0f},\tPA% = {100*avg_tool_time / avg:.1f}\t found {sum(found)}/5")
