3e1c443f1f898f4a706190e5755ec5e4c0bbd667
Markus Fleischmann <markus@losfleischmann.com>
2024-04-26 15:00:42 +0200

    Fast coverage merge broken

--- scripts/merge_coverage.py
+++ scripts/merge_coverage.py
@@ -6,12 +6,15 @@ for run in sys.argv[1:]:
     resfiles = list(os.listdir(out_dir))
     for tool in ["cbmc","esbmc","seahorn"]:
         files = []
+        total = 0
         for file in resfiles:
             if tool in file and not 'batches' in file:
+                total += 1
                 files.append(os.path.join(out_dir,file)) # For some reason filter + lambda does not work for this
                 file_string = ' --json-add-tracefile '.join(files)
-                outfile = f"{tool}_{len(files)}batches.cov.json"
-                cmd = f"python3 -m gcovr --json-add-tracefile {file_string}  --merge-mode-functions=separate --json-summary-pretty > {os.path.join(out_dir, outfile)}"
+                outfile = f"{tool}_{total}batches.cov.json"
+                tempfile = f"{tool}_{total}temp.cov.json"
+                cmd = f"python3 -m gcovr --json-add-tracefile {file_string}  --merge-mode-functions=separate --json {os.path.join(out_dir,tempfile)} --json-summary-pretty > {os.path.join(out_dir, outfile)}"
                 print(cmd)
                 subprocess.run(args=cmd, shell=True)
-                files = [outfile]
+                files = [os.path.join(out_dir,tempfile)]
