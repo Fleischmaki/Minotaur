63f365b5fba3f2e1dd7b67062dd8a4d4594f49b4
Fleischmaki <markus@losfleischmann.com>
2024-05-24 14:56:21 +0200

    Move arithmetic shifts into helper function, as signed shifts are implementation-dependent

--- src/maze_gen/smt2/formula_operations.py
+++ src/maze_gen/smt2/formula_operations.py
@@ -138,9 +138,7 @@ def get_shift_constraints(formula: FNode) -> list[FNode]:
     """ Returns constraints encoding that shifts cannot be larger than the width of the index  
     """
     shifts = get_nodes(formula, lambda f: f.is_bv_ashr() or f.is_bv_lshr() or f.is_bv_lshl())
-    return  [(shift.arg(1) < get_bv_width(shift)) for shift in shifts] + \
-            [BVSGE(shift.arg(0),BV(0,get_bv_width(shift))) for shift in filter(lambda s: s.is_bv_ashr(), shifts)]
-
+    return  [(shift.arg(1) < get_bv_width(shift)) for shift in shifts]
 def get_array_index_calls(formula: FNode):
     """ Collect all array calls and maximum index for formula
     """
