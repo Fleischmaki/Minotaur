57bc4155b9cd985422fc5465a44d0be5a20974d8
Markus Fleischmann <markus@losfleischmann.com>
2024-05-29 17:24:02 +0200

    Fix generate if no parameters passed

--- src/tools/generate.py
+++ src/tools/generate.py
@@ -7,7 +7,7 @@ def load(argv):
     gen = argv[0]
     out_dir = argv[1]
     param_string = argv[2:]
-    params = mg.get_params_from_string(" ".join(param_string))
+    params = {} if len(param_string) == 0 else mg.get_params_from_string(" ".join(param_string))
     if gen == 'container':
         mg.setup_generation_docker(params, out_dir, 0)
         mg.generate_maze_in_docker(params, 0).wait()
