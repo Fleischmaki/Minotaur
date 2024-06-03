53d9a5b01690e75a5bd7105e57fd49446c79f6af
Markus Fleischmann <markus@losfleischmann.com>
2024-06-03 10:28:53 +0200

    Fix build MC dockers

--- scripts/build_MC_dockers.sh
+++ scripts/build_MC_dockers.sh
@@ -10,6 +10,6 @@ fi
 
 DOCKERDIR=$(readlink -f $(dirname "$0")/..)/dockers
 for tool in base cpa-base cpa esbmc-base esbmc cbmc seahorn ultimate 2ls mopsa symbiotic;
-    do echo "[*] Build minotaur-$tool Docker image..."; cd "$DOCKERDIR/$tool"; docker build --network=host --ulimit nofile=1024 --rm -t --build-arg="CORE_COUNT=$CORE_COUNT" minotaur-$tool .; echo "[*] Done!";
+    do echo "[*] Build minotaur-$tool Docker image..."; cd "$DOCKERDIR/$tool"; docker build --network=host --ulimit nofile=1024 --rm --build-arg="CORE_COUNT=$CORE_COUNT" -t minotaur-$tool .; echo "[*] Done!";
 done; 
 echo "[*] Build minotaur-gen Docker image..."; cd "$DOCKERDIR/.."; docker build --ulimit nofile=1024 --network=host --rm -t minotaur-gen .; echo "[*] Done!";
