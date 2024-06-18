4b432edea2762026f66834a394021234323e6ea1
Markus Fleischmann <markus@losfleischmann.com>
2024-06-18 19:25:52 +0200

    Generate running at wrong log level

--- src/runner/commands.py
+++ src/runner/commands.py
@@ -16,7 +16,7 @@ def spawn_cmd(cmd_str: str, always_pipe_output=False):
     LOGGER.info('Executing: %s', cmd_str)
     cmd_args = cmd_str.split()
     try:
-        if not always_pipe_output and LOGGER.getEffectiveLevel() != logging.DEBUG:
+        if always_pipe_output or LOGGER.getEffectiveLevel() == logging.DEBUG:
             return subprocess.Popen(cmd_args, text=True)
         return subprocess.Popen(cmd_args, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, text=True)
     except Exception as e:
@@ -26,6 +26,6 @@ def spawn_cmd(cmd_str: str, always_pipe_output=False):
 def run_cmd(cmd_str: str, always_pipe_output=False):
     LOGGER.info('Executing: %s', cmd_str,)
     cmd_args = cmd_str.split()
-    if  not always_pipe_output or LOGGER.getEffectiveLevel() != logging.DEBUG:
-        return subprocess.run(cmd_args, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, check=False)
-    return subprocess.run(cmd_args, text=True, check=False)
+    if always_pipe_output or LOGGER.getEffectiveLevel() == logging.DEBUG:
+        return subprocess.run(cmd_args, text=True, check=False)
+    return subprocess.run(cmd_args, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL, check=False)
